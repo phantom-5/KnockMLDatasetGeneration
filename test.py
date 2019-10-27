b45997a1439585a6e1f1f6f61dd3054d822bd761
saswat@1998 <papalipratap@gmail.com>
2019-10-28 02:41:33 +0530

    ANN Model deployed and accuracy measured

--- test.py
+++ test.py
@@ -1,1 +1,51 @@
+# importing the libraries
+import numpy as np
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+
+#Splitting thedataset into training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#scaling done on the input data
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Buliding the ANN Model
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 4))
+    classifier.add(Dropout(p = 0.1))
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+    classifier.add(Dropout(p = 0.1))
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+    classifier.add(Dropout(p = 0.1))
+    classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 5, epochs = 150)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+mean = accuracies.mean()
+variance = accuracies.std()
+
+classifier.fit(X_train, y_train, batch_size = 5)
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+    
+    
 
